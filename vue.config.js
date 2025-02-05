bcca7ace7b3ca224a6446d5a9bf1de13755661ed
jslab-it <108869769+jslab-it@users.noreply.github.com>
2025-02-05 20:41:07 +0100

    fix

--- vue.config.js
+++ vue.config.js
@@ -5,5 +5,5 @@ module.exports = defineConfig({
   ]
 })
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/table_reservation/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/db/" : "/",
 };
